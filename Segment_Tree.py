16c48606fbc6a1c027132e9aba9f1b76a647f9c5
achilleasein <achilleasein@gmail.com>
2019-10-01 02:27:22 +0300

    Added cases to calculate the node value

--- Segment_Tree.py
+++ Segment_Tree.py
@@ -1,12 +1,26 @@
 class segment_node:
-    def __init__(self, left_child, right_child, parent, segment):
+    def __init__(self, left_child, right_child, parent, segment, value):
         self.left_child  = left_child
         self.right_child = right_child
         self.parent      = parent
         self.segment     = segment
+        self.value       = value
 
+    def calculate_value (self, value):
+        try:
+            value = int(right_child.value)
+        except:
+            pass
+        try:
+            value = int(left_child.value)
+        except:
+            pass
+        try:
+            value = int(right_child.value) + int(left_child.value)
+        except:
+            pass
 
-N1 = segment_node(None , None , None , 'A')
+N1 = segment_node(None , None , None , 'A', 0)
 
 
  
